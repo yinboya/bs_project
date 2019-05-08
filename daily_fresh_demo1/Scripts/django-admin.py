2beb2162be84851f5226691ef1bbdcbf67c53740
yin <1445128304@qq.com>
2019-05-08 09:37:25 +0800

    初始化代码

---
+++ daily_fresh_demo1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\without\Desktop\bs project\last\daily_fresh_demo\daily_fresh_demo1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
