688cc6df497fadbf76e3d53808847e76fabc2d61
yin <1445128304@qq.com>
2019-05-09 10:18:37 +0800

    yinboya

--- daily_fresh_demo/urls.py
+++ daily_fresh_demo/urls.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-# import xadmin
 from django.urls import path
 from django.conf.urls import url, include
 from django.views.static import serve  # 上传文件处理函数
 
+import xadmin
 from .settings import MEDIA_ROOT
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('xadmin/', xadmin.site.urls),
-
+    url(r'^xadmin/', xadmin.site.urls),
     url(r'^', include('df_goods.urls', namespace='df_goods')),
     url(r'^user/', include('df_user.urls', namespace='df_user')),
     url(r'^cart/', include('df_cart.urls', namespace='df_cart')),
