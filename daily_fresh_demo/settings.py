9aacf7c07e814923acd0a6925337f779ef0be0ee
xm <599597576@qq.com>
2019-05-11 00:29:33 +0800

    无用文件

--- daily_fresh_demo/settings.py
+++ daily_fresh_demo/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
         'NAME': 'last',       # 你要存储数据的库名，事先要创建之
         'USER': 'root',         # 数据库用户名
-        'PASSWORD': 'root',     # 密码
+        'PASSWORD': '123',     # 密码
         'HOST': '127.0.0.1',    # 主机
         'PORT': '3306',         # 数据库使用的端口
     }
